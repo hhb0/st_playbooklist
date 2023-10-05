f4675f07daab2b8f50f60a82485bca1421883c46
hhb0 <131655783+hhb0@users.noreply.github.com>
2023-10-05 17:25:36 +0900

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -152,6 +152,8 @@ with st.spinner(text="**책장에서 책을 꺼내오고 있습니다..📚**"):
 
         with c1:
             st.image(img_paths[st.session_state.idx%len(img_paths)])
+            st.write(st.session_state.idx%len(img_paths))
+            st.write(img_paths[st.session_state.idx%len(img_paths)])
             for index in range(len(result)):
                 img_url = result[index]['img_url']
                 title = result[index]['title']
